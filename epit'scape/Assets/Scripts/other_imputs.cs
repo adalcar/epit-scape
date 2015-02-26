9b28718484f353821961a4fcb991742671167465
adalcar <tancrede.erulin@gmail.com>
2015-02-26 10:01:42 +0100

    commence le script d'interaction

---
+++ epit'scape/Assets/Scripts/other_imputs.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class other_imputs : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Cancel"))
+            Application.Quit();
+	}
+}
---
+++ epit'scape/Assets/interaction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class interaction : MonoBehaviour {
+    private Ray interactRay;
+    private int range = 10;
+	// Use this for initialization
+	void Start () 
+    {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        interactRay.origin = transform.position;
+        interactRay.direction = transform.forward;
+	}
+}
