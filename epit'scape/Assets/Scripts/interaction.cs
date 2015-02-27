dbb26c3c12a16ec9bf7231e3394561070f8c2289
adalcar <tancrede.erulin@gmail.com>
2015-02-27 14:28:05 +0100

    ajoute un debut de script d'interaction et de gui

--- epit'scape/Assets/Scripts/interaction.cs
+++ epit'scape/Assets/Scripts/interaction.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class interaction : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private Ray interactRay;
+    private int range = 10;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        interactRay.origin = transform.position;
+        interactRay.direction = transform.forward;
+        RaycastHit interact;
+        if (Physics.Raycast(interactRay, out interact, range))
+        {
+            if (interact.transform.tag == "door")
+                transform.GetComponent<inGameGui>().Info("press x to open/close");
+        }
+    }
 }
---
+++ epit'scape/Assets/inGameGui.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inGameGui : MonoBehaviour {
+    public GUISkin skin;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Info(string infostring)
+    {
+        GUI.Label(new Rect(Screen.width / 2 - 40, Screen.height - 50, 80, 20), infostring);
+    }
+}
