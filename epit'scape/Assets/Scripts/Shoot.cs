cf5d856ce007586d653a73beb2c28bb555c49a8c
li <lieric@hotmail.fr>
2015-02-28 15:03:36 +0100

    lance banane script shoot F pour tirer

---
+++ epit'scape/Assets/Scripts/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public Transform bullet;
+	public GameObject posCanon;
+	public bool monArme = true;
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey("f") && monArme) {
+
+			Transform balle;
+			balle = Instantiate(bullet,posCanon.transform.position,posCanon.transform.rotation) as Transform;
+		    balle.rigidbody.AddForce(transform.forward * 1200);
+		}
+	
+	}
+}
