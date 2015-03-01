52b94385fe79f2816c81fb181b3651de44d78b27
adalcar <tancrede.erulin@gmail.com>
2015-03-01 23:48:01 +0100

    repost

---
+++ epit'scape/Assets/Scripts/gametime.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class gametime : MonoBehaviour {
+    int time;
+    private Dictionary<string,int> times;
+	// Use this for initialization
+	void Start () {
+        time = 0;
+        times = new Dictionary<string, int> { };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time++;
+	}
+    public void savetime(string label)
+    {
+        if (times.ContainsKey(label))
+        {
+            times.Remove(label);
+            times.Add(label, time);
+        }
+            
+    }
+    public int gettimesince(string label)
+    {
+        int savedtime;
+        if (times.TryGetValue(label, out savedtime))
+            return time - savedtime;
+        else 
+            return 0;
+    }
+}
+
---
+++ epit'scape/Assets/doorscript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorscript : MonoBehaviour {
+    public bool closed, locked;
+    Animation anim;
+	// Use this for initialization
+	void Start () 
+    {
+        anim = transform.GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    public void interact()
+    {
+        
+        if (!locked)
+        {
+            if (closed)
+            {
+                
+                anim.Play("DoorOpen");
+                closed = false;
+            }
+            else
+            {
+                anim.Rewind("DoorOpen");
+                closed = true;
+            }
+        }
+    }
+}
