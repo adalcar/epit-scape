ec0f7c4bc1ea481a03e6d30a4b36e40c7cd1f45b
li <lieric@hotmail.fr>
2015-03-01 11:45:29 +0100

    script shoot debugé cadence de tir

--- epit'scape/Assets/Scripts/Shoot.cs
+++ epit'scape/Assets/Scripts/Shoot.cs
@@ -5,21 +5,36 @@ public class Shoot : MonoBehaviour {
 
 	public Transform bullet;
 	public GameObject posCanon;
-	public bool monArme = true;
+    public bool monArme = true;
+
+	float cadence = 0.2f; 
 	
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKey("f") && monArme) {
+
+		if (Input.GetKey("f") && monArme ) {
 
 			Transform balle;
 			balle = Instantiate(bullet,posCanon.transform.position,posCanon.transform.rotation) as Transform;
-		    balle.rigidbody.AddForce(transform.forward * 1200);
+			balle.rigidbody.AddForce(transform.forward * 1200);
+			monArme = false;
+			StartCoroutine(StartWait());
+
 		}
+
 	
 	}
+	IEnumerator StartWait()
+	{   
+		yield return new WaitForSeconds(cadence);
+		monArme = true;
+
+	}
+
+
+
 }
