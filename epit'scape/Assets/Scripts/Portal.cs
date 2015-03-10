3dae5d3dbbc23271943ef93809af0d6d0cf558b6
adalcar <tancrede.erulin@gmail.com>
2015-03-10 23:07:02 +0100

    2-3 modifs, portal marche pas

---
+++ epit'scape/Assets/Scripts/Portal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour
+{
+    public string target;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void onTriggerEnter(Collider c)
+    {
+        Debug.Log("contact");
+        if (c.tag == "Player")
+        {
+            Application.LoadLevel(target);
+        }
+    }
+}
