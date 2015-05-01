562525d7b6a979872525901017bc6cb12a766225
li <lieric@hotmail.fr>
2015-05-01 13:57:48 +0200

    level 0 mise de VB et de la cafet

--- epit'scape/Assets/Scripts/QuestManager.cs
+++ epit'scape/Assets/Scripts/QuestManager.cs
@@ -1,28 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class QuestManager : MonoBehaviour 
+public class QuestManager : MonoBehaviour
 {
-	void Update () 
+    void Update()
     {
-	}
+    }
 
     void OnTriggerEnter(Collider coll)
     {
-        if( coll.tag == "Player" /*&& Input.GetButtonDown("x")*/)
+        if (coll.tag == "Player")
         {
-            if(!GetComponent<Quest>().isStarted)
+            if (!GetComponent<Quest>().isStarted)
             {
                 GetComponent<Quest>().isStarted = true;
                 StartCoroutine(coll.GetComponentInChildren<inGameGui>().Info("tu vas en chier"));
             }
 
-            if(GetComponent<Quest>().isFinished)
+            if (GetComponent<Quest>().isFinished)
             {
-                if(this.name == "Porte")
+                string name = this.name;
+                switch (name)
+                {
+                    case "Porte":
+                        Application.LoadLevel("Level 01");
+                        break;
+                    case "PO-1":
+                        Application.LoadLevel("Level 01");
+                        break;
+                }
+
+
+                /*if(this.name == "Porte")
                 {
                     Application.LoadLevel("Level 01");
                 }
+                if(this.name == "P0-1")
+                {
+                    Application.LoadLevel("Level 02");
+                }*/
             }
         }
     }
