c9b47124748656373c4c02c8d14ff6023c6534a5
BananasPropellersNumb <fabien-wade@orange.fr>
2015-06-27 23:10:40 +0200

    Update credits

---
+++ epit'scape/Assets/Scripts/Credits.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+    public GameObject cameraF;
+    public GameObject cameraE;
+    public GameObject image;
+    public int speed;
+    public int LimitTime;
+    public string level;
+	
+	
+	void Update () 
+    {
+        GameObject camera = Choose();
+        camera.transform.Translate(Vector3.down * Time.deltaTime * speed);
+        image.transform.Translate(Vector3.down * Time.deltaTime * speed);
+        StartCoroutine(WaitFor());
+	}
+
+    IEnumerator WaitFor()
+    {
+        yield return new WaitForSeconds(LimitTime);
+        Application.LoadLevel(level);
+    }
+
+    GameObject Choose()
+    {
+        if (Biblio.english == true)
+        {
+            return cameraE;
+        }
+        else
+        {
+            return cameraF;
+        }
+    }
+}
